fe4579c0981bba567382b175803daef79d36225a
Nikolay Markov <enchantner@gmail.com>
2013-08-05 22:47:14 +0400

    refactoring and mongodb

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 import queue
 import itertools
 import threading
+import argparse
 from urllib.parse import urljoin
 
 from grab import Grab
+from pymongo import MongoClient
 
 
 class Crawler(threading.Thread):
@@ -14,39 +16,53 @@ class Crawler(threading.Thread):
         super(Crawler, self).__init__()
         self.urls = urls
         self.stop = threading.Event()
-        self.tries = 0
-        self.maxtries = maxtries
         self.g = Grab()
-        self.maxdepth = maxdepth
+
+        self.tries = 0
         self.depth = 0
+        self.maxtries = maxtries
+        self.maxdepth = maxdepth
 
         self.baseurl = "http://en.wikipedia.org/"
+        self.db = MongoClient('localhost', 27017)
+        self.wiki = self.db.wiki
+
+    def update_links(self, page):
+        if not self.depth >= self.maxdepth:
+            links = (
+                l.get("href").split("#")[0]
+                for l in page.cssselect("a[href]")
+            )
+            self.depth += 1
+            list(map(self.add_url, links))
 
     def add_url(self, href):
-        clean_href = href.split("#")[0]
-        if clean_href.startswith("/wiki"):
-            self.urls.put(urljoin(self.baseurl, clean_href))
+        url = urljoin(self.baseurl, href)
+        exist = self.wiki.parsed.find_one({"url": url})
+        if not exist and href.startswith("/wiki"):
+            self.urls.put(url)
+
+    def save(self, url, page):
+        entry = {
+            "url": url,
+            "content": page.cssselect("#mw-content-text")[0].text_content()
+        }
+        self.wiki.parsed.insert(entry)
 
     def run(self):
         while True:
             try:
-                msg = self.urls.get(timeout=5)
-                print(msg)
+                url = self.urls.get(timeout=5)
+                print(url)
             except queue.Empty:
                 self.tries += 1
                 if self.tries > self.maxtries:
                     print("Nothing to do here, exiting...")
                     break
 
-            resp = self.g.go(msg)
-            links = (
-                l.get("href")
-                for l in self.g.tree.cssselect("a[href]")
-            )
-            if not self.depth >= self.maxdepth:
-                self.depth += 1
-                list(map(self.add_url, links))
-
+            resp = self.g.go(url)
+            self.save(url, self.g.tree)
+            self.update_links(self.g.tree)
 
 def main():
     urls = queue.Queue()
